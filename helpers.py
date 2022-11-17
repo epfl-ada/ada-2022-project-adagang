06ab085db494a13a6878d90ccd51837660e5194b
Majdoulineait <majdouline.aityahia@epfl.ch>
2022-11-17 22:05:01 +0100

    remove non-english tiles and descriptions

--- helpers.py
+++ helpers.py
@@ -1,3 +1,50 @@
+##=========================================Remove non-English words================================================
+
+# Set of negative emojis
+negative_emojis = ["\U0001F92E","\U0001F922","\U0001F623","\U0001F624","\U0001F621","\U0001F620","\U0001F92C","\U0001F608","\U0001F47F","\U0001F480","\U00002620","\U0001F4A9","\U0001F921","\U0001F479","\U0001F47A","\U0001F4A5","\U0001F4A2","\U0001F595","\U0001F44E","\U0001F52A","\U0001F9E8","\U0001FA93","\U0001F5E1","\U0001F51E","\U000026D4","\U00001F6AB","\U00002623","\U00002622","\U00002757","\U0000203C","\U00002049","\U0001F4DB"]
+
+def isEnglish(w):
+    return w.isascii() or w in negative_emojis
+
+def english_or_emoji(text):
+    words = set(text.split(' '))
+    nb_en_words_or_emojis = sum(1 for w in words if English(w) else 0)
+    nb_words = len(words)
+    percentage = nb_en_words_or_emojis / nb_words
+    return percentage
+
+f = open('yt_metadata_en.jsonl')
+videos = ijson.items(f, '', multiple_values=True)
+
+list_new_data = []
+
+for video in tqdm(videos):    
+    percentage_title = english_or_emoji(video['title'])
+    percentage_des = english_or_emoji(video['description'])
+    
+    list_new_data.append({
+        'percentage_title': percentage_title,
+        'percentage_des': percentage_des
+    })
+        
+print('Finished')  
+f.close()
+
+# Convert the list of dicts to a dataframe
+%%time
+w = pd.read_parquet('generated/videos_few_columns.parquet', engine='fastparquet')
+newcols = pd.DataFrame.from_dict(list_new_data)
+# Join the dataframe of videos with the newly copmuted columns
+joined = w.join(newcols)
+# Write the joined dataframe to a file
+joined.to_parquet('generated/videos_CountNegWordsTitle.parquet', compression=None)
+
+# When analysing on the title: keep titles where the percentage is above 60%
+w = w[w['percentage_title'] > 0.6]
+
+# When analysing on the descriptions: keep descriptions where the percentage is above 60%
+w = w[w['percentage_title'] > 0.6]
+
 ##=========================================UPPERCASE AND EXCLAMATION================================================
 
 # Helper function to count the number of capitalized words and exclamation marks in a text
@@ -9,9 +56,6 @@ def count_words(text):
     nb_words = len(words)
     return nb_upper_words, nb_exc_marks, nb_words
 
-# Count the upper words and exclamation marks in titles and descriptions 
-f = open('yt_metadata_en.jsonl')
-videos = ijson.items(f, '', multiple_values=True)
 
 list_new_data = []
 
@@ -32,9 +76,6 @@ print('Finished')
 f.close()
 
 ##======================================NEGATIVE EMOJIS==================================================
-# Set of negative emojis
-neg_emojis = set(["\U0001F52B","\U0001F92E","\U0001F922","\U0001F623","\U0001F624","\U0001F621","\U0001F620","\U0001F92C","\U0001F608","\U0001F47F","\U0001F480","\U00002620","\U0001F4A9","\U0001F921","\U0001F479","\U0001F47A","\U0001F4A5","\U0001F4A2","\U0001F595","\U0001F44E","\U0001F52A","\U0001F9E8","\U0001FA93","\U0001F5E1","\U0001F51E","\U000026D4","\U0001F6AB","\U00002623","\U00002622","\U00002757","\U0000203C","\U00002049","\U0001F4DB"])
-
 
 # function to count number of negative emojis in a text
 def count_neg_emojis(text):
