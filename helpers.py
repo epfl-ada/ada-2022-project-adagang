decd336ae1666049d7916389455600f9edd736bc
Walfar <victor.carles@epfl.ch>
2022-11-18 23:37:39 +0100

    added comments for helpers.py and moved to exploration.ipynb

--- helpers.py
+++ helpers.py
@@ -10,9 +10,14 @@ from parallelbar import progress_imap
 # Set of negative emojis
 negative_emojis = {"\U0001F92E","\U0001F922","\U0001F623","\U0001F624","\U0001F621","\U0001F620","\U0001F92C","\U0001F608","\U0001F47F","\U0001F480","\U00002620","\U0001F4A9","\U0001F921","\U0001F479","\U0001F47A","\U0001F4A5","\U0001F4A2","\U0001F595","\U0001F44E","\U0001F52A","\U0001F9E8","\U0001FA93","\U0001F5E1","\U0001F51E","\U000026D4","\U00001F6AB","\U00002623","\U00002622","\U00002757","\U0000203C","\U00002049","\U0001F4DB"}
 
+# After analyzing the videos, we detected the existence of weird characters in the titles and descriptions that could interfere with our
+# study. We decided to code this function that will check if a given word is english, by checking if it has ascii characters, or negative
+# emoji to fit in our study.
 def isEnglish(w):
     return w.isascii() or w in negative_emojis
 
+# This function checks for each word of the given text if it is english. We compute a percentage of english words in the text, that couls
+# used as a threshold to filter our dataframe.
 def english_or_emoji(text):
     words = set(text.split(' '))
     nb_en_words_or_emojis = sum(1 for w in words if isEnglish(w))
